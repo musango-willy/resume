a660ad8f9ed9f18cf27d9604a285336fab6e65d0
musango willy <musangowilly@gmail.com>
2023-10-15 00:05:13 +0300

    js variables

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //learning javascript is interesting.
 
 let age=20;
-let isAdult =age>=18?"yes"
+let isAdult =age>=18?"yes":"no";
 console.log(isAdult);
+
+let firstName="wycliffe";
+let lastName="musango";
+console.log(firstName);
+console.log(lastName);
